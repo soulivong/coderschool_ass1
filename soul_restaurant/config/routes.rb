28c8932f540e2ce9d376706e793a358131b200a4
soulivong <soulivongfc@gmail.com>
2016-10-15 00:40:45 +0700

    add framework and basic installation

--- soul_restaurant/config/routes.rb
+++ soul_restaurant/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  get 'contact_us' => 'home#contact'
+
+  get 'menu' => 'home#menu'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
